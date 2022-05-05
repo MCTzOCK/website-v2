cf12fae08a0ba861ef844798124b6aa823b78b45
MCTzOCK <dev@mctzock.de>
2022-05-05 21:29:22 +0200

    Added SEO Description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,7 @@ function App({Component, pageProps}: AppProps) {
         <>
             <DefaultSeo
                 title={"Ben Siebert - Software Engineer & Student"}
+                description={"Ben Siebert is a software engineer and former student at the Gymnasium Holthausen in Hattingen, Germany. He is passionate about web development and is currently working on his own projects."}
                 openGraph={{
                     type: 'website',
                     locale: 'en_US',
