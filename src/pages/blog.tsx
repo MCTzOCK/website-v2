b891e53d5be9dfcc98b4dac2593c21137c15fa29
MCTzOCK <dev@mctzock.de>
2022-05-08 16:29:20 +0200

    Updated Blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,138 +1,155 @@
 import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-  Box,
-  Flex,
-  Heading,
-  Input,
-  SimpleGrid,
-  Stack,
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle,
+    Box,
+    Flex,
+    Heading,
+    Input,
+    SimpleGrid,
+    Stack, Text,
 } from '@chakra-ui/react';
-import { NextSeo } from 'next-seo';
-import { useState } from 'react';
+import {NextSeo} from 'next-seo';
 import BlogCard from '../components/BlogCard';
-import { getAllFilesFrontMatter } from '../lib/posts';
+import {getAllFilesFrontMatter} from '../lib/posts';
+import {useEffect, useState} from "react";
 
-const Blog = ({ posts }: { posts: any }) => {
-  const [postsFiltered, setPostsFiltered] = useState(posts);
+const Blog = ({posts}: { posts: any }) => {
+    const [postsFiltered, setPostsFiltered] = useState(posts);
 
-  return (
-    <>
-      <NextSeo title="Blog | Ben Siebert" description="Browse my Blogs. Read something about me and my personal thoughts about technology" />
-      <Stack>
-        <Box
-          d="flex"
-          alignItems={'center'}
-          justifyContent={'center'}
-          p="2.5rem"
-          w="100%"
-          bgColor={'messenger.900'}
-        >
-          <Input
-            id="blog-search-field"
-            placeholder="Search..."
-            w={['80%']}
-            bg="white"
-            color={'black'}
-            _placeholder={{ color: 'black' }}
-            onChange={(v) => {
-              const search = v.target.value.toLowerCase();
-              if (search.startsWith('tag:')) {
-                const tag = search.replace('tag:', '');
-                setPostsFiltered(
-                  posts.filter((post: any) =>
+    function rerender(search: string) {
+        if (search.startsWith('tag:')) {
+            const tag = search.replace('tag:', '');
+            setPostsFiltered(
+                posts.filter((post: any) =>
                     post.tags.join(',').toLowerCase().includes(tag)
-                  )
-                );
-                return;
-              }
-              if (search.startsWith('author:')) {
-                const author = search.replace('author:', '');
-                setPostsFiltered(
-                  posts.filter((post: any) =>
+                )
+            );
+            return;
+        }
+        if (search.startsWith('author:')) {
+            const author = search.replace('author:', '');
+            setPostsFiltered(
+                posts.filter((post: any) =>
                     post.author.toLowerCase().includes(author)
-                  )
-                );
-                return;
-              }
-              if (search.startsWith('date:')) {
-                const date = search.replace('date:', '');
-                setPostsFiltered(
-                  posts.filter((post: any) =>
+                )
+            );
+            return;
+        }
+        if (search.startsWith('date:')) {
+            const date = search.replace('date:', '');
+            setPostsFiltered(
+                posts.filter((post: any) =>
                     post.publishedAt.toLowerCase().includes(date)
-                  )
-                );
-                return;
-              }
-              if (search.startsWith('title:')) {
-                const title = search.replace('title:', '');
-                setPostsFiltered(
-                  posts.filter((post: any) =>
+                )
+            );
+            return;
+        }
+        if (search.startsWith('title:')) {
+            const title = search.replace('title:', '');
+            setPostsFiltered(
+                posts.filter((post: any) =>
                     post.title.toLowerCase().includes(title)
-                  )
-                );
-                return;
-              }
-              if (search.startsWith('summary:')) {
-                const summary = search.replace('summary:', '');
-                setPostsFiltered(
-                  posts.filter((post: any) =>
+                )
+            );
+            return;
+        }
+        if (search.startsWith('summary:')) {
+            const summary = search.replace('summary:', '');
+            setPostsFiltered(
+                posts.filter((post: any) =>
                     post.summary.toLowerCase().includes(summary)
-                  )
-                );
-                return;
-              }
-              setPostsFiltered(
-                posts.filter((post: any) => {
-                  return (
+                )
+            );
+            return;
+        }
+        setPostsFiltered(
+            posts.filter((post: any) => {
+                return (
                     post.title.toLowerCase().includes(search) ||
                     post.author.toLowerCase().includes(search) ||
                     post.summary.toLowerCase().includes(search) ||
                     post.tags.join(',').toLowerCase().includes(search)
-                  );
-                })
-              );
-            }}
-          />
-        </Box>
-        {postsFiltered.length === 0 ? (
-          <>
-            <Alert status="error">
-              <AlertIcon />
-              <AlertTitle>No Blogs found!</AlertTitle>
-              <AlertDescription>
-                No Blog Posts were found matching your request.
-              </AlertDescription>
-            </Alert>
-          </>
-        ) : null}
-        <SimpleGrid minChildWidth={'320px'} spacing={8}>
-          {postsFiltered.map((post: any) => (
-            <BlogCard
-              key={post.slug}
-              title={post.title}
-              subtitle={post.author}
-              description={post.summary}
-              date={post.publishedAt}
-              firstButton={{ href: '/blog/' + post.slug, text: 'Read more' }}
-              tags={post.tags}
-            />
-          ))}
-        </SimpleGrid>
-      </Stack>
-    </>
-  );
+                );
+            })
+        );
+    }
+
+    useEffect(() => {
+        const x = setInterval(() => {
+            const search = (document.getElementById('blog-search-field') as HTMLInputElement).value;
+            rerender(search);
+        }, 100);
+        return () => clearInterval(x);
+    }, [])
+
+    return (
+        <>
+            <NextSeo title="Blog | Ben Siebert"
+                     description="Browse my Blogs. Read something about me and my personal thoughts about technology"/>
+            <Stack>
+                <Box
+                    d="flex"
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    p="2.5rem"
+                    w="100%"
+                    flexDirection={"column"}
+                    bgColor={'twitter.600'}
+                >
+                    <Heading size={"xl"} color={"gray.900"}>Blog</Heading>
+                    <Text mb={"1rem"} color={"gray.900"} textAlign={"center"}>
+                        In my blog you can read about my personal thoughts about technology and my life.
+                    </Text>
+                    <Input
+                        id="blog-search-field"
+                        placeholder="Search..."
+                        w={['80%']}
+                        bg="white"
+                        color={'black'}
+                        _placeholder={{color: 'black'}}
+                        onChange={(v) => {
+                            rerender(v.target.value.toLowerCase());
+                        }}
+                    />
+                </Box>
+                {postsFiltered.length === 0 ? (
+                    <>
+                        <Alert status="error">
+                            <AlertIcon/>
+                            <AlertTitle>No Blogs found!</AlertTitle>
+                            <AlertDescription>
+                                No Blog Posts were found matching your request.
+                            </AlertDescription>
+                        </Alert>
+                    </>
+                ) : null}
+                <SimpleGrid minChildWidth={'320px'} spacing={8}>
+                    {postsFiltered.map((post: any) => (
+                        <BlogCard
+                            key={post.slug}
+                            title={post.title}
+                            subtitle={post.author}
+                            description={post.summary}
+                            date={post.publishedAt}
+                            firstButton={{href: '/blog/' + post.slug, text: 'Read more'}}
+                            tags={post.tags}
+                        />
+                    ))}
+                </SimpleGrid>
+            </Stack>
+        </>
+    );
 };
 
 export async function getStaticProps() {
-  const data = await getAllFilesFrontMatter('blog');
-  const posts = data.sort(
-    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
-  );
+    const data = await getAllFilesFrontMatter('blog');
+    const posts = data.sort(
+        (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+    );
 
-  return { props: { posts } };
+    return {props: {posts}};
 }
 
 export default Blog;
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,169 +1,174 @@
-import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/router';
+import {NextSeo} from 'next-seo';
+import {useRouter} from 'next/router';
 import {
-  Box,
-  Flex,
-  Heading,
-  Text,
-  Link as ChakraLink,
-  useColorModeValue,
-  Icon,
+    Box,
+    Flex,
+    Heading,
+    Text,
+    Link as ChakraLink,
+    useColorModeValue,
+    Icon,
 } from '@chakra-ui/react';
-import { MDXProvider } from '@mdx-js/react';
-import { MdEdit } from 'react-icons/md';
+import {MDXProvider} from '@mdx-js/react';
+import {MdEdit} from 'react-icons/md';
 import dayjs from 'dayjs';
 import hydrate from 'next-mdx-remote/hydrate';
-import { getFileBySlug, getFiles } from '../../lib/posts';
+import {getFileBySlug, getFiles} from '../../lib/posts';
 import Image from 'next/image';
 import CodeBlock from '../../components/md/CodeBlock';
 import {
-  CustomLink,
-  InlineCode,
-  ListItem,
-  OrderedList,
-  UnorderedList,
+    CustomLink,
+    InlineCode,
+    ListItem,
+    OrderedList,
+    UnorderedList,
 } from '../../components/md/common';
 
 const BlogPost = ({
-  mdxSource,
-  frontMatter,
-}: {
-  mdxSource: any;
-  frontMatter: any;
+                      mdxSource,
+                      frontMatter,
+                  }: {
+    mdxSource: any;
+    frontMatter: any;
 }) => {
-  const { push } = useRouter();
+    const {push} = useRouter();
 
-  const color = useColorModeValue('gray.700', 'gray.400');
+    const color = useColorModeValue('gray.700', 'gray.400');
 
-  const content = hydrate(mdxSource, {
-    components: {
-      // @ts-ignore
-      h1: (props: any) => (
-        <Heading size="xl" {...props}>
-          {props.children}
-        </Heading>
-      ),
-      // @ts-ignore
-      h2: (props: any) => (
-        <Heading size="lg" {...props}>
-          {props.children}
-        </Heading>
-      ),
-      // @ts-ignore
-      h3: (props: any) => (
-        <Heading size="md" {...props}>
-          {props.children}
-        </Heading>
-      ),
-      // @ts-ignore
-      h4: (props: any) => (
-        <Heading size="sm" {...props}>
-          {props.children}
-        </Heading>
-      ),
-      // @ts-ignore
-      h5: (props: any) => (
-        <Heading size="sm" {...props}>
-          {props.children}
-        </Heading>
-      ),
-      // @ts-ignore
-      h6: (props: any) => (
-        <Heading size="sm" {...props}>
-          {props.children}
-        </Heading>
-      ),
+    const content = hydrate(mdxSource, {
+        components: {
+            // @ts-ignore
+            h1: (props: any) => (
+                <Heading size="xl" {...props}>
+                    {props.children}
+                </Heading>
+            ),
+            // @ts-ignore
+            h2: (props: any) => (
+                <Heading size="lg" {...props}>
+                    {props.children}
+                </Heading>
+            ),
+            // @ts-ignore
+            h3: (props: any) => (
+                <Heading size="md" {...props}>
+                    {props.children}
+                </Heading>
+            ),
+            // @ts-ignore
+            h4: (props: any) => (
+                <Heading size="sm" {...props}>
+                    {props.children}
+                </Heading>
+            ),
+            // @ts-ignore
+            h5: (props: any) => (
+                <Heading size="sm" {...props}>
+                    {props.children}
+                </Heading>
+            ),
+            // @ts-ignore
+            h6: (props: any) => (
+                <Heading size="sm" {...props}>
+                    {props.children}
+                </Heading>
+            ),
 
-      // @ts-ignore
-      a: CustomLink,
-      // @ts-ignore
-      ul: UnorderedList,
-      // @ts-ignore
-      ol: OrderedList,
-      // @ts-ignore
-      li: ListItem,
-      // @ts-ignore
-      inlineCode: InlineCode,
-      // @ts-ignore
-      code: CodeBlock,
-    },
-  });
+            // @ts-ignore
+            p: (props: any) => (
+                <Text fontSize="1.6rem" {...props} />
+            ),
 
-  const title = frontMatter.title;
-  const description = frontMatter.summary;
-  const url = `https://ben-siebert.de/blog/${frontMatter.slug}`;
+            // @ts-ignore
+            a: CustomLink,
+            // @ts-ignore
+            ul: UnorderedList,
+            // @ts-ignore
+            ol: OrderedList,
+            // @ts-ignore
+            li: ListItem,
+            // @ts-ignore
+            inlineCode: InlineCode,
+            // @ts-ignore
+            code: CodeBlock,
+        },
+    });
 
-  return (
-    <>
-      <NextSeo
-        title={frontMatter.title + ' | Ben Siebert - Blog'}
-        description={frontMatter.summary}
-        openGraph={{
-          type: 'website',
-          locale: 'en_US',
-          url: 'https://ben-siebert.de/blog/' + frontMatter.slug,
-          site_name: frontMatter.title + ' | Ben Siebert - Blog',
-          images: [
-            {
-              url: 'https://ben-siebert.de/assets/brand/cartoon.jpg',
-              width: 512,
-              height: 512,
-              alt: 'Ben Siebert - Blog',
-              type: 'image/jpg',
-            },
-          ],
-        }}
-        twitter={{
-          handle: '@OfficialMCTzOCK',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <MDXProvider>
-        <Box
-          as="section"
-          px={{ md: '10', lg: '20', xl: '40' }}
-          py="4"
-          fontSize="16px"
-          marginInline={'.5rem'}
-        >
-          <Box as="header" textAlign="center">
-            <Heading as="h1" py="4" size="2xl">
-              {frontMatter.title}
-            </Heading>
+    const title = frontMatter.title;
+    const description = frontMatter.summary;
+    const url = `https://ben-siebert.de/blog/${frontMatter.slug}`;
 
-            <Flex direction="column">
-              <Text fontSize="16px" color={color} py="1">
-                {frontMatter.author} /{' '}
-                {dayjs(frontMatter.publishedAt).format('MMMM DD, YYYY')} /{' '}
-                {frontMatter.readingTime.text}
-              </Text>
-            </Flex>
-          </Box>
-          <Box as="article">{content}</Box>
-        </Box>
-      </MDXProvider>
-    </>
-  );
+    return (
+        <>
+            <NextSeo
+                title={frontMatter.title + ' | Ben Siebert - Blog'}
+                description={frontMatter.summary}
+                openGraph={{
+                    type: 'website',
+                    locale: 'en_US',
+                    url: 'https://ben-siebert.de/blog/' + frontMatter.slug,
+                    site_name: frontMatter.title + ' | Ben Siebert - Blog',
+                    images: [
+                        {
+                            url: 'https://ben-siebert.de/assets/brand/cartoon.jpg',
+                            width: 512,
+                            height: 512,
+                            alt: 'Ben Siebert - Blog',
+                            type: 'image/jpg',
+                        },
+                    ],
+                }}
+                twitter={{
+                    handle: '@OfficialMCTzOCK',
+                    cardType: 'summary_large_image',
+                }}
+            />
+            <MDXProvider>
+                <Box
+                    as="section"
+                    px={{md: '10', lg: '20', xl: '40'}}
+                    py="4"
+                    fontSize="16px"
+                    marginInline={'.5rem'}
+                >
+                    <Box as="header" textAlign="center">
+                        <Heading as="h1" py="4" size="2xl">
+                            {frontMatter.title}
+                        </Heading>
+
+                        <Flex direction="column">
+                            <Text fontSize="16px" color={color} py="1">
+                                {frontMatter.author} /{' '}
+                                {dayjs(frontMatter.publishedAt).format('MMMM DD, YYYY')} /{' '}
+                                {frontMatter.readingTime.text}
+                            </Text>
+                        </Flex>
+                    </Box>
+                    <Box as="article">{content}</Box>
+                </Box>
+            </MDXProvider>
+        </>
+    );
 };
 
 export const getStaticPaths = async () => {
-  const posts = await getFiles('blog');
+    const posts = await getFiles('blog');
 
-  return {
-    paths: posts.map((post) => ({
-      params: {
-        slug: post.replace(/\.mdx/, ''),
-      },
-    })),
+    return {
+        paths: posts.map((post) => ({
+            params: {
+                slug: post.replace(/\.mdx/, ''),
+            },
+        })),
 
-    fallback: false,
-  };
+        fallback: false,
+    };
 };
 
-export const getStaticProps = async ({ params }: { params: any }) => {
-  const post = await getFileBySlug('blog', params.slug);
+export const getStaticProps = async ({params}: { params: any }) => {
+    const post = await getFileBySlug('blog', params.slug);
 
-  return { props: post };
+    return {props: post};
 };
 
 export default BlogPost;
